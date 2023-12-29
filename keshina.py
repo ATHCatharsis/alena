a39986a962fd59afd94056239a87c74ee88b08ea
ATHCatharsis <155149774+ATHCatharsis@users.noreply.github.com>
2023-12-29 10:58:27 +0100

    Create keshina.py

---
+++ keshina.py
@@ -0,0 +1,60 @@
+def func1(arg1, arg2):
+    var27 = func2(arg1, arg2)
+    var28 = -528 + 2054245427 ^ (arg2 - -119) ^ (arg2 - (var27 & (((var27 ^ arg2) | var27 ^ (var27 ^ -671 & (616465984 | -1837665899) - (arg2 + arg2) + arg2)) & (612 ^ -488) + 160061703 & arg2 - 431))) - arg2
+    var29 = (arg1 + 104 + arg2) ^ 548271250
+    var30 = 2 - (var27 - var27)
+    var31 = var27 & var28
+    result = -762519608 - var27
+    return result
+def func2(arg3, arg4):
+    var10 = var7(arg3, arg4)
+    var11 = func8()
+    var16 = func9(var10, arg4)
+    var21 = func10(var10, var11)
+    var22 = (((arg4 - arg4) + ((var11 + var16 & (arg4 & 647503859 - (((829 - var16) | (var21 | (var16 ^ arg3))) | (arg4 | -538 - var11 ^ (arg4 + -951)) ^ arg3) ^ 2068455937)) - arg4)) ^ var11 | -604) - arg4
+    var23 = var21 | var10 & (var10 ^ var16)
+    var24 = var21 + (arg4 | var23 + var11 - ((var21 & (arg4 ^ var11 | (arg4 & ((var10 + arg4) | arg4)) | (-877 | (var11 - (var23 + var23 ^ (((var23 ^ var22) | 1288430957) - -1499844887)))) & -1908284119)) + var11 & var10))
+    var25 = (var21 - (((var22 - 797 & var23) - arg3) - 81595013 - 890 - arg4 + var21 - (var16 ^ ((1724932377 | ((717833349 | var23 ^ -616) & var21 - 160) + var24 | var11) & var11 - var16 & -979476073)))) | var16
+    var26 = (arg3 | 955245064 - var23 + ((var25 - -1217143236 ^ var23 ^ var16) - var23 & var24)) - var24
+    result = (arg4 - var10) - var24 & (((var25 & ((arg3 + (1307733796 | var11)) ^ var25)) | var10) ^ -1619566456 + 765703187 + var24)
+    return result
+def func10(arg17, arg18):
+    var19 = 0
+    for var20 in range(18):
+        var19 += arg17 | arg17 - arg17
+    return var19
+def func9(arg12, arg13):
+    var14 = 0
+    for var15 in xrange(24):
+        var14 += (var15 | var14) - -8
+    return var14
+def func8():
+    func6()
+    result = len(range(28))
+    func7()
+    return result
+def func7():
+    global len
+    del len
+def func6():
+    global len
+    len = lambda x : -6
+def func5(arg8, arg9):
+    result = -711 + (arg8 | 525) - -683666280
+    return result
+def func4():
+    closure = [-3]
+    def func3(arg5, arg6):
+        closure[0] += func5(arg5, arg6)
+        return closure[0]
+    func = func3
+    return func
+var7 = func4()
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 11'
+    print 'arg_number: 32'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x
